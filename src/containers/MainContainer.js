13acdcb9a8826765dfab7dce94d4309d96e031fe
github username <github email address>
2019-05-28 15:07:07 -0400

    search forms and results page

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,54 +1,21 @@
 import React from 'react';
 import Map from '../components/Map';
 import TripContainer from './TripContainer'
+import SearchResults from './SearchResults.js';
 
 
 class Main extends React.Component {
-
-    constructor() {
-        super()
-        this.state = {
-
-        }
-    }
-
+    
     render() {
         return (
             <div className="main-container">
+                {console.log('main container props', this.props)}
+                <SearchResults searchTerms={this.props.searchTerms}/>
                 <TripContainer />
             </div>
         )
     }
 
-    // constructor(props) {
-    //     super(props)
-    //     this.state= {
-    //         mapCoords: {LNG: -85, LAT: 35, ZOOM: 5},
-    //         citySearch: ''
-    //     }
-    // }
-
-    // moveMap= (lng, lat, zoom)=>{
-    //     this.setState({ mapCoords: {
-    //         LNG: lng,
-    //         LAT: lat,
-    //         ZOOM: zoom
-    //     }})
-    // }
-
-    // render() {
-    //     return(
-    //         <div className='App'>
-    //             {console.log('MAIN RENDERED')}
-    //             <div className='item1'>
-    //                 <EventsList />
-    //             </div>
-    //             {/* <div className='item2'> */}
-    //                 {/* <Map mapCoords={this.state.mapCoords} moveMap={this.moveMap}/> */}
-    //             {/* </div> */}
-    //         </div>
-    //     )
-    // }
 }
 
 export default Main
