5acdb6faa0e701553b0cdd9cbe2782e7957e4b63
github username <github email address>
2019-05-28 15:05:35 -0400

    resync time

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+          <div className="ui menu">
+              <Link to='/'className='item'>
+                <h1 className="ui header">
+                  <img src='trippin_logo.png' id="logo" alt="oops"></img>
+                </h1>
+              </Link>
+              <Link to='/about'className='item'>
+                  <div className="content">About</div>
+              </Link>
+              <Link to='/budget'className='item'>
+                  <div className="content">Budget</div>
+              </Link>
+              {
+                  localStorage.getItem("token") ?
+                  <Link to="/" className="position right item" style={{ color: 'black' }}
+                  onClick={(e) => localStorage.clear() } >
+                  <div className="content">Logout</div>
+                  </Link> :
+                  null
+                  // <Link to="/login" className="position right item" style={{ color: 'black' }} >
+                  // <div className="content">Login</div>
+                  // </Link>
+              }
+          </div>
+            )
+}
+
+export default Header;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Map from '../components/Map';
+import LoginForm from '../components/LoginForm';
 import TripContainer from './TripContainer'
 
 
@@ -14,41 +14,70 @@ class Main extends React.Component {
 
     render() {
         return (
-            <div className="main-container">
-                <TripContainer />
+            <div className="ui container">
+            {
+                localStorage.getItem("token") ?
+                <div className="ui two column grid">
+                    <div className="ten wide column">
+                        <TripContainer />
+                    </div>
+                    <div className="six wide column">
+                        <TripContainer />
+                    </div>
+                </div> :
+                    <LoginForm history={this.props.history}/>
+            }
             </div>
         )
     }
-
-    // constructor(props) {
-    //     super(props)
-    //     this.state= {
-    //         mapCoords: {LNG: -85, LAT: 35, ZOOM: 5},
-    //         citySearch: ''
-    //     }
-    // }
-
-    // moveMap= (lng, lat, zoom)=>{
-    //     this.setState({ mapCoords: {
-    //         LNG: lng,
-    //         LAT: lat,
-    //         ZOOM: zoom
-    //     }})
-    // }
-
-    // render() {
-    //     return(
-    //         <div className='App'>
-    //             {console.log('MAIN RENDERED')}
-    //             <div className='item1'>
-    //                 <EventsList />
-    //             </div>
-    //             {/* <div className='item2'> */}
-    //                 {/* <Map mapCoords={this.state.mapCoords} moveMap={this.moveMap}/> */}
-    //             {/* </div> */}
-    //         </div>
-    //     )
-    // }
 }
 
-export default Main
+export default Main;
+
+
+
+
+
+
+            // <div className="ui container">
+            //     <div className="ui two column grid">
+            //         <div className="ten wide column">
+            //             <TripContainer />
+            //         </div>
+            //         <div className="six wide column">
+            //             <TripContainer />
+            //         </div>
+            //     </div>
+            // </div>
+        //     )
+        // }
+    
+        // constructor(props) {
+        //     super(props)
+        //     this.state= {
+        //         mapCoords: {LNG: -85, LAT: 35, ZOOM: 5},
+        //         citySearch: ''
+        //     }
+        // }
+    
+        // moveMap= (lng, lat, zoom)=>{
+        //     this.setState({ mapCoords: {
+        //         LNG: lng,
+        //         LAT: lat,
+        //         ZOOM: zoom
+        //     }})
+        // }
+    
+        // render() {
+        //     return(
+        //         <div className='App'>
+        //             {console.log('MAIN RENDERED')}
+        //             <div className='item1'>
+        //                 <EventsList />
+        //             </div>
+        //             {/* <div className='item2'> */}
+        //                 {/* <Map mapCoords={this.state.mapCoords} moveMap={this.moveMap}/> */}
+        //             {/* </div> */}
+        //         </div>
+        //     )
+        // }
